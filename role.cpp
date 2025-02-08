f35d90fefec66b992fcce1de1f09b7e32323c468
illiyeen25 <jameelm@student.douglascollege.ca>
2025-02-07 21:08:54 -0800

    removed role.cpp from my branch

--- role.cpp
+++
@@ -1,8 +0,0 @@
-class Role
-{
-    enum class Roles { RegFaculty, TempFaculty, Chair, Dean };
-    Roles role; /* = RegFaculty; */
-public:
-    Role(std::string newRole);
-    std::string getRoleName() const; /* "RegFaculty", "TempFaculty", "Chair", "Dean" */
-};
