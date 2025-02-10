1dd61c7a920d0f77aad892819013a108387c056a
Illiyeen25 <jameelm@student.douglascollege.ca>
2025-02-09 21:13:22 -0800

    Made the formatting even better.

--- User.cpp
+++ User.cpp
@@ -16,30 +16,30 @@ class User
     std::string getRole() const;
 };
 User::User(std::string name, std::string pwd, std::string rle)
-    {
-        username = name;
-        password = pwd;
-        role = rle;
+{
+    username = name;
+    password = pwd;
+    role = rle;
         
-    }
-    bool User::login() const
-    {   
-        std::fstream credentials;
-        std::string line;
-        std::string all_credentials = username;
-        all_credentials.append(" ").append(password).append(" ").append(role.getRoleName()).append("\n");
-        credentials.open("credentials.txt");
-        while(getline(credentials,line))
-        {
-           if (all_credentials == line) break;
-        }
-        return (all_credentials == line);
-    }
-    bool* User::getPermission() const
-    { 
-        return role.getPermissions();
-    }
-    std::string User::getRole() const
+}
+bool User::login() const
+{   
+    std::fstream credentials;
+    std::string line;
+    std::string all_credentials = username;
+    all_credentials.append(" ").append(password).append(" ").append(role.getRoleName()).append("\n");
+    credentials.open("credentials.txt");
+    while(getline(credentials,line))
     {
-        return role.getRoleName();
+        if (all_credentials == line) break;
     }
+    return (all_credentials == line);
+}
+bool* User::getPermission() const
+{ 
+    return role.getPermissions();
+}
+std::string User::getRole() const
+{
+    return role.getRoleName();
+}
